593f2fdfd082ca741f28145fdea0ff34181dd693
zalmareli <79586395+zalmareli@users.noreply.github.com>
2023-07-21 16:15:13 -0300

    Se modifico la funcion indexView para mostrar las categorias y las publicaciones en la home

--- src/core/views.py
+++ src/core/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Categoria, Articulo
 
 # vista de la pagina de inicio
+
+
 def indexView(request):
-    return render(request, 'core/index.html', {})
+    # Obtener las últimas publicaciones
+    ultimas_publicaciones = Articulo.objects.filter(publicado=True)[:5]
+
+    # Obtener todas las categorías y etiquetas
+    categorias = Categoria.objects.all()
+
+     # Pasar los datos como contexto al renderizado de la plantilla
+    context = {
+        'ultimas_publicaciones': ultimas_publicaciones,
+        'categorias': categorias,
+    }
+
+    return render(request, 'core/index.html', context)
+
 
 def aboutView(request):
     return render(request, 'core/about.html', {})
+
 
 def contactView(request):
     return render(request, 'core/contact.html', {})
