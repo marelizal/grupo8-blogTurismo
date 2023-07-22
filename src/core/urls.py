5db7061ab3b80bd940297af0a0f50b20e3278244
mpieszko <mpieszko@hotmail.com>
2023-07-22 14:58:54 -0300

    gallery 2

--- src/core/urls.py
+++ src/core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.indexView, name = 'index'),
     path('acerca_de_nosotros', views.aboutView, name = 'about'),
     path('contacto', views.contactView, name = 'contact'),
+    path('gallery', views.galleryView, name = 'gallery'),
 
     # Inclu
     path('publicaciones/', include('posts.urls')),
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from posts.models import Categoria,Articulo
 
@@ -27,3 +29,9 @@ def aboutView(request):
 
 def contactView(request):
     return render(request, 'core/contact.html', {})
+
+def galleryView(request):
+    media_path = os.path.join(settings.MEDIA_ROOT,'articulo')  # Obtiene la ruta absoluta de la carpeta /media
+    image_files = [f for f in os.listdir(media_path) if os.path.isfile(os.path.join(media_path, f))]
+    images = [os.path.join(settings.MEDIA_URL, 'articulo', f) for f in image_files]
+    return render(request, 'core/gallery.html', {'images': images})
