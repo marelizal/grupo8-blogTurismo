82df6db0fb7278f18028e09261568451c2a4c6ec
mpieszko <mpieszko@hotmail.com>
2023-07-21 21:48:13 -0300

    config media 1

--- src/blog/settings/base.py
+++ src/blog/settings/base.py
@@ -118,3 +118,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
