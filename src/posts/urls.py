828b1f422eb86a28c5d46f19d4b99088f595162b
zalmareli <79586395+zalmareli@users.noreply.github.com>
2023-07-22 15:05:17 -0300

    se agrego la vista y funcionalidad add_post

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
-   
-
+    path('add', views.post_add, name='post_add')
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-from .models import Articulo,Categoria
+from django.shortcuts import render, redirect
+from .forms import ArticuloForm
+from .models import Articulo, Categoria
+
 
 def post_list(request):
     post = Articulo.objects.all()
@@ -10,16 +10,32 @@ def post_list(request):
 
     context = {
         'posts': post,
-        "categorias":categorias
+        "categorias": categorias
     }
     return render(request, 'posts/post_list.html', context)
+
 
 def post_detail(request, pk):
     post = Articulo.objects.get(pk=pk)
     categorias = Categoria.objects.all()
 
     context = {
-        "post": post,  
+        "post": post,
         "categorias": categorias
     }
     return render(request, 'posts/post_detail.html', context)
+
+
+
+def post_add(request):
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST, request.FILES)
+        if form.is_valid():
+            articulo = form.save(commit=False)
+            articulo.autor = request.user  
+            articulo.save()
+            return redirect('post_detail', pk=articulo.pk)
+    else:
+        form = ArticuloForm()
+    
+    return render(request, 'posts/post_add.html', {'form': form})
