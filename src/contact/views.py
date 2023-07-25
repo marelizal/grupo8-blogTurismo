f9012790e18c4eee487b5505d5de0f98d2319d83
Miguel Gomez <miguegomez2706@gmail.com>
2023-07-24 21:41:57 -0300

    configuracion para que mande al correo, mejora visual de la vista contacto

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from .models import Contacto
 # Create your views here.
 
@@ -9,5 +10,12 @@ def contactoView(request):
         mensaje = request.POST.get('mensaje')
         contact = Contacto(nombre=nombre, email=email, mensaje=mensaje)
         contact.save()
+
+        # Enviar correo electrónico
+        subject = 'Nuevo mensaje de contacto'
+        message = f'Nombre: {nombre}\nEmail: {email}\nMensaje: {mensaje}'
+        from_email = 'tchaco.soporte@gmail.com'  # Cambia por tu dirección de correo electrónico
+        recipient_list = ['tchaco.soporte@gmail.com']  # Cambia por la dirección de correo del destinatario
+        send_mail(subject, message, from_email, recipient_list)
 
     return render(request, 'core/contact.html', {})
