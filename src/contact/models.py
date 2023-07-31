88736f3b0fa4f3b0aba453f51e61e85654685596
zalmareli <79586395+zalmareli@users.noreply.github.com>
2023-07-31 08:21:07 -0300

    Se agregaron opciones al formulario y contacto

--- src/contact/models.py
+++ src/contact/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-# Create your models here.
 
+# Create your models here.
 class Contacto(models.Model):
-    nombre = models.CharField(max_length = 100)
+    nombre = models.CharField(max_length=100)
     email = models.EmailField()
+    asunto = models.CharField(max_length=200)
+    tipo_consulta = models.CharField(max_length=50, choices=[
+        ('pregunta_general', 'Pregunta general'),
+        ('sugerencia', 'Sugerencia'),
+        ('reportar_problema', 'Reportar un problema'),
+    ])
     mensaje = models.TextField()
 
     def __str__(self):
--- src/contact/views.py
+++ src/contact/views.py
@@ -2,25 +2,24 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib import messages
 from .models import Contacto
-# Create your views here.
 
 def contactoView(request):
     if request.method == 'POST':
         nombre = request.POST.get('nombre')
         email = request.POST.get('email')
+        asunto = request.POST.get('asunto')
+        tipo_consulta = request.POST.get('tipo_consulta')
         mensaje = request.POST.get('mensaje')
-        contact = Contacto(nombre=nombre, email=email, mensaje=mensaje)
-        contact.save()
 
         # Enviar correo electrónico
-        subject = 'Nuevo mensaje de contacto'
-        message = f'Nombre: {nombre}\nEmail: {email}\nMensaje: {mensaje}'
+        subject = f'Nuevo mensaje de contacto - {asunto}'
+        message = f'Nombre: {nombre}\nEmail: {email}\nTipo de consulta: {tipo_consulta}\nMensaje: {mensaje}'
         from_email = 'tchaco.soporte@gmail.com'
         recipient_list = ['tchaco.soporte@gmail.com']
         send_mail(subject, message, from_email, recipient_list)
 
-          # Mostrar mensaje flash
+        # Mostrar mensaje de éxito
         messages.success(request, 'El mensaje se envió correctamente. ¡Gracias por contactarnos!')
-        return redirect('contacto')  # Redireccionar a la misma página para mostrar la alerta
+        return redirect('contacto')
 
     return render(request, 'core/contact.html', {})
